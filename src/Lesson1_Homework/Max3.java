f5e9c7e57fa01f69fd60632e50834ed64c0c6145
aOsokin <a1exsander@mail.ru>
2018-10-19 12:27:23 +0300

    Task#7

---
+++ src/Lesson1_Homework/Max3.java
@@ -0,0 +1,10 @@
+package Lesson1_Homework;
+
+public class Max3 {
+    public static void main(String[] args) {
+        int max=0, a=123;
+        max = (a%100/10>=a%10) ? a%100/10 : a%10;
+        max = (a/100>max) ? a/100 : max;
+        System.out.println(max);
+    }
+}
