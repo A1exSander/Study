96cd2b8068431eb117e58ad670d8eb645a54388a
aOsokin <a1exsander@mail.ru>
2018-10-19 13:16:21 +0300

    Task#6

--- src/Lesson1_Homework/SQRT.java
+++ src/Lesson1_Homework/SQRT.java
@@ -3,7 +3,7 @@ package Lesson1_Homework;
 public class SQRT {
     public static void main(String[] args) {
         double a=12,b=7,c=1, disc,x=0,x2=0;
-        // Присваиваю ноль как индикатор отсутствия корней. Да-да, если с=0, то это и будут корни, но без условного оператора не знаю, как сделать лучше
+        // Присваиваю ноль как индикатор отсутствия корней
         disc=b*b-4*a*c;
         x = (disc==0) ? -b/(2*a) : (disc>0) ? (-b+Math.sqrt(disc))/(2*a) : x;
         x2 = (disc>0) ? (-b-Math.sqrt(disc))/(2*a) : x2;
