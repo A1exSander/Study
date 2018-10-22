32dc50496513a4817b3678111e0b71e0abaa8968
aOsokin <a1exsander@mail.ru>
2018-10-22 03:28:01 +0300

    Homework 2 - tasks 1-7

--- src/Lesson2_Homework/Homework.java
+++ src/Lesson2_Homework/Homework.java
@@ -17,6 +17,7 @@ public class Homework {
 //    Возрастающая последовательность: -5, 0, 7
         final int a=1, b=5, c=-10;
         int first, second, third;
+        System.out.println("Задание 1");
         if (a<b && a<c) {
             first=a;
             if (b<c){
@@ -45,41 +46,48 @@ public class Homework {
                 third = a;
             }
         }
-            System.out.println("Упорядоченная последовательность чисел: "+first+" "+second+" "+third);
-
+        System.out.println("Упорядоченная последовательность чисел: "+first+" "+second+" "+third);
+        System.out.println();
 
 //        Задача 2
 //        Создайте программу, выводящую на экран все неотрицательные элементы последовательности 90 85 80 75 70 65 60 ….
+        System.out.println("Задание 2");
         System.out.println("Список чисел последовательности: ");
         for (int i=90; i>0; i=i-5){
             System.out.println(i);
         }
+        System.out.println();
 
 
 //        Задача 3*
 //        Создайте программу, выводящую на экран первые 20 элементов последовательности 2 4 8 16 32 64 128 ….
         int counter3=2;
+        System.out.println("Задание 3");
         for (int i=1; i<21; i++){
             System.out.println("Элеемент №" + i + ": " + counter3);
             counter3=counter3*2;
         }
+        System.out.println();
 
 
 //        Задача 4
 //        Проверьте, является ли натуральное число — простым.
 //        (Метод перебора делителей)
         int number=59, j=0, div=2;
+        System.out.println("Задание 4");
         while ((j!=1) && (div*div<=number)) {
             if (number%div==0) j=1;
             div++;
             }
         if (j==0) System.out.println("Число простое");
         else System.out.println("Число составное");
+        System.out.println();
 
 
 //        Задача 5
 //        Выведите на экран первые 11 членов последовательности Фибоначчи. Первый и второй члены последовательности равны единицам, а каждый следующий — сумме двух предыдущих.
         int fib1=1, fib2=1, fib;
+        System.out.println("Задание 5");
         System.out.println("Последовательность Фибоначчи:");
         System.out.println(fib1);
         System.out.println(fib2);
@@ -89,6 +97,8 @@ public class Homework {
             fib2=fib;
             System.out.println(fib);
         }
+        System.out.println();
+
 
 
 //        Задача 6*
@@ -99,11 +109,29 @@ public class Homework {
 //        билета и теперь раздумывает, как много сувениров потребуется. С помощью программы подсчитайте
 //        сколько счастливых билетов в одном рулоне?
         int happyCounter=0;
+        System.out.println("Задание 6");
         for (int ticketNum=1; ticketNum<1000000; ticketNum++){
             if (ticketNum/100000 + ticketNum%100000/10000+ ticketNum%10000/1000 == ticketNum%1000/100 + ticketNum%100/10 + ticketNum%10) happyCounter++;
         }
         System.out.println("Количество счастливых битетов: " + happyCounter);
+        System.out.println();
 
+
+//        Задача 7*
+//        Электронные часы показывают время в формате от 00:00 до 23:59. Подсчитать сколько раз за сутки случается так,
+//        что слева от двоеточия показывается симметричная комбинация для той, что справа от двоеточия (например, 02:20, 11:11 или 15:51).
+        System.out.println("Задача 7");
+//        В сутках 1440 минут
+        int hourCounter=0;
+        int sym, minutes, hours;
+        for (int i=0; i<1441; i++){
+            minutes=i%60;
+            hours=i/60;
+            sym=minutes%10*10+minutes/10;
+            if (hours==sym) hourCounter++;
+        }
+        System.out.println("Количество симметричных комбинаций: " + hourCounter);
+        System.out.println();
     }
 }
 
