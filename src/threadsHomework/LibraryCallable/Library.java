fa154903ef21c58e7c0f3291b4b62dd7a271b9aa
aOsokin <a1exsander@mail.ru>
2019-01-09 23:08:44 +0300

    Library done

--- src/threadsHomework/LibraryCallable/Library.java
+++ src/threadsHomework/LibraryCallable/Library.java
@@ -19,30 +19,32 @@ class Library {
         } else lib.putIfAbsent(book, quantity);
     }
 
-    void takeBook(Book book, Integer quantity, boolean takingHome) {
+    int takeBook(Book book, Integer quantity, boolean takingHome) {
         if (takingHome == book.getPermissionStatus() || !takingHome) {
             if (lib.containsKey(book) && lib.get(book) > 0 && lib.get(book) - quantity >= 0) {
                 int q = lib.get(book);
                 if (lib.get(book) - quantity == 0) {
                     lib.remove(book);
-                    System.out.println("Взято" + quantity + "экземпляров книги " + book.toString());
+                    System.out.println("Взято " + quantity + " экземпляров книги " + book.toString());
                     System.out.println("Экземпляров этой книги в библиотеке больше нет");
+                    return quantity;
                 } else {
                     lib.put(book, q - quantity);
                     System.out.println("Взято " + quantity + " экземпляров книги " + book.toString());
+                    return quantity;
                 }
-            } else
+            } else {
                 System.out.println("Этой книги нет в библиотеке или запрошенное количество превышает имеющееся в наличии");
+                return 0;
+            }
+
         } else System.out.println("Эту книгу нельзя забрать домой");
+        return 0;
     }
 
-    void returnBooks(Map<Book, Integer> takenHome, Map<Book, Integer> takenLib) {
-        for (Map.Entry entry: takenHome.entrySet()) {
-            lib.merge((Book) entry.getKey(),(Integer) entry.getValue(), (oldVal, newVal) -> oldVal + newVal);
-        }
-        for (Map.Entry entry: takenLib.entrySet()) {
-            lib.merge((Book) entry.getKey(),(Integer) entry.getValue(), (oldVal, newVal) -> oldVal + newVal);
-        }
+    void returnBooks(Book book, int quantity) {
+        if (lib.containsKey(book)) lib.merge(book, quantity, (oldVal, newVal) -> oldVal + newVal);
+        else lib.put(book, quantity);
     }
 
     ConcurrentMap<Book, Integer> getLib(){
--- src/threadsHomework/LibraryCallable/Reader.java
+++ src/threadsHomework/LibraryCallable/Reader.java
@@ -22,46 +22,55 @@ public class Reader implements Runnable {
 
         Scanner scanner = new Scanner(System.in);
         while(true) {
-            library.showLib();
-            System.out.println("Введите номер книги, которую хотите забрать");
-            int index = scanner.nextInt();
-            System.out.println("Введите количество книг, которые хотите забрать");
-            quantity = scanner.nextInt();
-            System.out.println("Вы хотите забрать домой? (true/false)");
-            takeHome = scanner.nextBoolean();
+            try {
+                library.showLib();
+                System.out.println("Введите номер книги, которую хотите забрать");
+                int index = scanner.nextInt();
+                System.out.println("Введите количество книг, которые хотите забрать");
+                quantity = scanner.nextInt();
+                System.out.println("Вы хотите забрать домой? (true/false)");
+                takeHome = scanner.nextBoolean();
+                int q = 0;
+                boolean flag = false;
 
 
+                for (Book book : library.getLib().keySet()) {
+                    if (index == book.getIndex()) {
+                        if (takeHome && book.getPermissionStatus()) {
+                            q = library.takeBook(book, quantity, takeHome);
+                            if (q > 0) takenHome.put(book, q);
+
+                        } else if (takeHome && !book.getPermissionStatus()) {
+                            System.out.println("Книгу нельзя взять домой");
+                        } else {
+                            q = library.takeBook(book, quantity, takeHome);
+                            if (q > 0) takenLib.put(book, q);
+
+                        }
+                        flag = true;
+                    }
+
+                }
+                if (!flag) System.out.println("Этой книги нет в библиотеке");
 
 
-            for (Book book : library.getLib().keySet()) {
-                   if (index == book.getIndex()) {
-                       if (takeHome && book.getPermissionStatus()) {
-                           takenHome.put(book, quantity);
-                           library.takeBook(book, quantity, takeHome);
-                       }
-                       else if (takeHome && !book.getPermissionStatus()) {
-                           System.out.println("Книгу нельзя взять домой");
-                       }
-                       else {
-                           takenLib.put(book, quantity);
-                           library.takeBook(book, quantity, takeHome);
-                       }
-
-               }
-            }
-
-            System.out.println("Хотите взять еще одну книгу? (true/false)");
-            boolean more = scanner.nextBoolean();
-            if (!more) {
-                break;
-            } else {
-                System.out.println("Видимо, хотите еще");
+                System.out.println("Хотите взять еще одну книгу? (true/false)");
+                boolean more = scanner.nextBoolean();
+                if (!more) {
+                    break;
+                } else {
+                    System.out.println("Хорошо");
+                    System.out.println();
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Вы ввели неверные данные");
+                return;
             }
         }
 
         System.out.println("Книги, взятые домой:");
         for (Map.Entry entry: takenHome.entrySet()) {
-            System.out.println(entry);
+            System.out.println (entry);
         }
 
         System.out.println("Книги, взятые в читальный зал:");
@@ -72,24 +81,48 @@ public class Reader implements Runnable {
 
         boolean returnBook = scanner.nextBoolean();
         if (returnBook) {
-            library.returnBooks(takenHome, takenLib);
+            while (true) {
+                try {
+                    System.out.println("Введите индекс книги, которую хотите вернуть");
+                    int index = scanner.nextInt();
+                    System.out.println("Сколько экземпляров возвращаете?");
+                    quantity = scanner.nextInt();
 
-//            while (true) {
-//                System.out.println("Введите индекс книги, которую хотите вернуть");
-//                int index = scanner.nextInt();
-//                System.out.println("Сколько экземпляров возвращаете?");
-//                quantity = scanner.nextInt();
-//
-//
-//                System.out.println("Хотите вернуть еще одну книгу?");
-//                if (scanner.nextLine().toLowerCase().equals("да")) {
-//                } else if (scanner.nextLine().toLowerCase().equals("нет")) {
-//                    break;
-//                } else {
-//                    System.out.println("Видимо, это значит нет");
-//                    break;
-//                }
-//            }
+                    for (Map.Entry entry : takenLib.entrySet()) {
+                        Book book = (Book) entry.getKey();
+                        if (book.getIndex() == index) {
+                            if (quantity <= (Integer) entry.getValue() && quantity > 0) {
+                                library.returnBooks(book, quantity);
+                            } else
+                                System.out.println("У вас нет столько экземпляров этой книги или вы указали число меньше единицы");
+                        }
+                    }
+
+                    for (Map.Entry entry : takenHome.entrySet()) {
+                        Book book = (Book) entry.getKey();
+                        if (book.getIndex() == index) {
+                            if (quantity <= (Integer) entry.getValue() && quantity > 0) {
+                                library.returnBooks(book, quantity);
+                            } else
+                                System.out.println("У вас нет столько экземпляров этой книги или вы указали число меньше единицы");
+                        }
+                    }
+
+
+                    System.out.println("Хотите вернуть еще одну книгу?");
+
+                    boolean more = scanner.nextBoolean();
+                    if (!more) {
+                        break;
+                    } else {
+                        System.out.println("Хорошо");
+                        System.out.println();
+                    }
+                } catch (InputMismatchException e) {
+                System.out.println("Вы ввели неверные данные");
+                return;
+                }
+            }
         }
         System.out.println("Всего хорошего, " + name + ". Заходите еще.");
 
